b88b6f1bec7b7e6b01a1b3c5aa1bc4a33c8c651c
shikhar229 <shikharagarwale7@gmail.com>
2025-10-26 01:42:54 +0530

    added attack function for chaotic monkey

--- src/MonkeyNft.sol
+++ src/MonkeyNft.sol
@@ -11,13 +11,14 @@ contract MonkeyNft is ERC721, VRFConsumerBaseV2Plus {
     error MonkeyNft__ONLY_MONKE();
     error MonkeyNft__InvalidRequestId();
     error MonkeyNft__UnauthorizedTransfer();
+    error MonkeyNft__AlreadyGuarded();
+    error MonkeyNft__AttackOnCooldown();
 
     // enums
     enum MonkeyType {
         CHAOTIC,
         FARMER,
-        GUARD,
-        REGULAR
+        GUARDIAN
     }
 
     enum Rarity {
@@ -34,6 +35,8 @@ contract MonkeyNft is ERC721, VRFConsumerBaseV2Plus {
         Rarity rarity;
         uint256 farmingPower;
         uint256 creationTimestamp;
+        bool isGuarded;
+        uint256 attackCooldown;
     }
 
     struct VrfConfig {
@@ -56,6 +59,8 @@ contract MonkeyNft is ERC721, VRFConsumerBaseV2Plus {
     mapping(uint256 tokenId => MonkeyTraits) private s_monkeyInfo;
     mapping(address owner => uint256[] tokenIds) public s_monkeyOwnerToTokenIds;
     mapping(uint256 requestId => address owner) public s_mintRequests;
+    mapping(uint256 requestId => uint256 attackerTokenId) public s_attackRequests;
+    mapping(uint256 guardTokenId => uint256 tokenId) public s_guardedMonkeys;
 
     modifier onlyMonke {
         if (msg.sender != MONKE) {
@@ -98,7 +103,21 @@ contract MonkeyNft is ERC721, VRFConsumerBaseV2Plus {
         uint256[] calldata _randomWords
     ) internal override {
         require(s_mintRequests[_requestId] != address(0), MonkeyNft__InvalidRequestId());
+
+        if (s_mintRequests[_requestId] != address(0)) {
+            _mintMonkeyNft(_requestId, _randomWords);
+        }
+        else if (s_attackRequests[_requestId] != 0) {
+            _attackMonkeyFulfillRequest(_requestId, _randomWords);
+        }
+        else {
+            revert MonkeyNft__InvalidRequestId();
+        }
+    }
+
+    function _mintMonkeyNft(uint256 _requestId, uint256[] calldata _randomWords) internal {
         address monkeyOwner = s_mintRequests[_requestId];
+        s_mintRequests[_requestId] = address(0);
         _safeMint(monkeyOwner, s_tokenCounter);
         uint256 randomWord = _randomWords[0];
         uint256 tokenId = s_tokenCounter++;
@@ -112,11 +131,64 @@ contract MonkeyNft is ERC721, VRFConsumerBaseV2Plus {
             monkeyType: MonkeyType(monkeyType),
             rarity: Rarity(rarity),
             farmingPower: farmingPower,
-            creationTimestamp: block.timestamp
+            creationTimestamp: block.timestamp,
+            isGuarded: false,
+            attackCooldown: 0
         });
 
         s_monkeyOwnerToTokenIds[monkeyOwner].push(tokenId);
         emit MonkeyMinted(monkeyOwner, tokenId);
+    }
+
+    function guardMonkey(uint256 guardTokenId, uint256 tokenId) external {
+        require(s_guardedMonkeys[guardTokenId] == 0, MonkeyNft__AlreadyGuarded());
+        MonkeyTraits memory guardMonkeyInfo = s_monkeyInfo[guardTokenId];
+        require(guardMonkeyInfo.monkeyType == MonkeyType.GUARDIAN && guardMonkeyInfo.owner == msg.sender, MonkeyNft__ONLY_MONKE());
+        
+        MonkeyTraits memory targetMonkey = s_monkeyInfo[tokenId];
+        require(!targetMonkey.isGuarded, MonkeyNft__AlreadyGuarded());
+        s_monkeyInfo[tokenId].isGuarded = true;
+    }
+
+    function attackMonkey(uint256 attackerTokenId) external returns (uint256 requestId) {
+        MonkeyTraits memory attackerMonkeyInfo = s_monkeyInfo[attackerTokenId];
+        require(attackerMonkeyInfo.monkeyType == MonkeyType.CHAOTIC && attackerMonkeyInfo.owner == msg.sender, MonkeyNft__ONLY_MONKE());
+        require(attackerMonkeyInfo.attackCooldown <= block.timestamp, MonkeyNft__AttackOnCooldown());
+
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: s_vrfConfig.keyHash,
+                subId: s_vrfConfig.subId,
+                requestConfirmations: s_vrfConfig.requestConfirmations,
+                callbackGasLimit: s_vrfConfig.callbackGasLimit,
+                numWords: s_vrfConfig.numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: true}))
+            })
+        );
+
+        s_attackRequests[requestId] = attackerTokenId;
+        s_monkeyInfo[attackerTokenId].attackCooldown = type(uint256).max;
+        emit MonkeyMintRequested(requestId, msg.sender, block.timestamp);
+    }
+
+    function _attackMonkeyFulfillRequest(uint256 _requestId, uint256[] calldata _randomWords) internal {
+        uint256 attackerTokenId = s_attackRequests[_requestId];
+        s_attackRequests[_requestId] = 0;
+
+        uint256 randomWord = _randomWords[0];
+        uint256 targetTokenId = (randomWord % (s_tokenCounter - 1)) + 1;
+
+        MonkeyTraits memory targetMonkey = s_monkeyInfo[targetTokenId];
+        if (targetMonkey.isGuarded) {
+            s_monkeyInfo[targetTokenId].isGuarded = false;
+            s_monkeyInfo[attackerTokenId].attackCooldown = block.timestamp + 2 days;
+        } else {
+            s_monkeyInfo[attackerTokenId].attackCooldown = block.timestamp + 1 days;
+            uint256 reduceFarmingPowerBy = randomWord % 10;
+            if (s_monkeyInfo[targetTokenId].farmingPower > reduceFarmingPowerBy) {
+                s_monkeyInfo[targetTokenId].farmingPower -= reduceFarmingPowerBy;
+            }
+        }
     }
 
     function _update(address _to, uint256 _tokenId, address _auth) internal override returns (address) {
